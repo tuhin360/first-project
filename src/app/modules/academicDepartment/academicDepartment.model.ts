2a93da737f55973ce2a7cecc14736247fdde3f9d
tuhin <tuhin153695@gmail.com>
2023-12-25 10:46:46 +0600

    academic department interface, model and validation added

---
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+import { TAcademicDepartment } from './academicDepartment.interface';
+
+const academicDepartmentSchema = new Schema<TAcademicDepartment>(
+  {
+    name: {
+      type: 'string',
+      required: true,
+      unique: true,
+    },
+    academicfaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AcademicDepartment = model<TAcademicDepartment>(
+  'AcademicDepartment',
+  academicDepartmentSchema,
+);
